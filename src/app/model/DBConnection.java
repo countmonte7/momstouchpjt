c5eb25b361839db69940e931fe986c2377bef3a2
countmonte7 <countmonte7@naver.com>
2019-07-06 15:10:24 +0900

    fixed db security issue

--- src/app/model/DBConnection.java
+++ src/app/model/DBConnection.java
@@ -1,17 +1,24 @@
 package app.model;
 
+import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Properties;
+import java.util.ResourceBundle;
 
 public class DBConnection {
 		private static Connection conn;
 		
 		public static Connection db() {	
 			try {
+				Properties prop = new Properties();
+				FileInputStream ip = new FileInputStream("C:/spring/momsTouch/src/config.properties");
+				prop.load(ip);
 				Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
 				String url = "jdbc:mysql://localhost:3306/momstouch?characterEncoding=UTF-8&&serverTimezone=UTC";
-				String userId = "root";
-				String userPw = "47690eo5dh";
+				String userId = prop.getProperty("dbId");
+				String userPw = prop.getProperty("dbPw");
+				System.out.println(userId);
 				conn = DriverManager.getConnection(url, userId, userPw);
 				
 			}catch(Exception e) {
